c1daa0f4751a0f43796c339ec805c87e09181f88
Marc J <john.doe.gmail.com>
2020-11-19 11:06:25 +0100

    Taak1.14

--- src/main/java/be/vdab/frituurfrida/controllers/SausController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SausController.java
@@ -11,6 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("sauzen")
@@ -33,6 +35,31 @@ class SausController {
         ModelAndView modelAndView = new ModelAndView("saus");
         Arrays.stream(sauzen).filter(saus -> saus.getNummer() == id).findFirst()
                 .ifPresent(saus -> modelAndView.addObject("saus", saus));
+        return modelAndView;
+    }
+
+    private List<Character> beginLetters(){
+        return Arrays.stream(sauzen).map(saus -> saus.getNaam().toLowerCase().charAt(0))
+                .distinct().sorted().collect(Collectors.toList());
+    }
+
+    @GetMapping("alfabet")
+    public ModelAndView alfabet(){
+        return new ModelAndView("alfabet", "beginletters", beginLetters());
+
+    }
+
+    private List<Saus> sauzenMetLetter(Character letter){
+        return Arrays.stream(sauzen)
+                .filter(saus -> saus.getNaam().toLowerCase().charAt(0) == letter)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("alfabet/{letter}")
+    public ModelAndView sausMetLetter(@PathVariable Character letter){
+        ModelAndView modelAndView =
+                new ModelAndView("alfabet", "sauzen",sauzenMetLetter(letter));
+        modelAndView.addObject("beginletters", beginLetters());
         return modelAndView;
     }
 }
